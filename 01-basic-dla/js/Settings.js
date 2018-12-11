f6906b0375c2cd323b820924f1a3971346efa73d
Jason Webb <zen.webb@gmail.com>
2018-12-10 20:56:10 -0600

    Documenting Settings, disabling bias effect for 01

--- 01-basic-dla/js/Settings.js
+++ 01-basic-dla/js/Settings.js
@@ -1,5 +1,13 @@
 export default {
+  // Diameter of particles
   CircleDiameter: 5,
+
+  // Method to use to set up initial cluster particles. Can be Point, Ring, or Random
   InitialClusterType: 'Random',
-  UseFrame: true
+
+  // Confine sketch to 900x900 area
+  UseFrame: true,
+
+  // Disable bias effect
+  BiasTowards: ''
 };
