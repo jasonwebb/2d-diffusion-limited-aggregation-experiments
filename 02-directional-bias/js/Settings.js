ede6748871a07e6941e54f1768a56c099d824778
Jason Webb <zen.webb@gmail.com>
2018-12-12 18:36:22 -0600

    Removing unused settings from 02

--- 02-directional-bias/js/Settings.js
+++ 02-directional-bias/js/Settings.js
@@ -2,15 +2,9 @@ export default {
   // Diameter of particles
   CircleDiameter: 5,
 
-  // Method to use to set up initial cluster particles. Can be Point, Ring, Random, or Wall
-  InitialClusterType: 'Random',
-
   // Confine sketch to frame (900x900px by default)
   UseFrame: true,
 
   // Hide walkers by default
   ShowWalkers: false,
-
-  // Enable bias effect
-  BiasTowards: 'Bottom'
 };
